c072493054714472ffbb857a5ad8bad5e632d4a7
S.K.A.I <dhrumilnpatel01@gmail.com>
2026-06-28 18:47:46 +0530

    Update web.py

--- web.py
+++ web.py
@@ -165,20 +165,20 @@ preferences, ratings, genres, and viewing behavior.
     st.header("🔄 How It Works")
 
     st.info("""
-    1️⃣ User Enters Movie Details
+        1️⃣ User Enters Movie Details
 
-    ⬇
+                    ⬇
 
-    2️⃣ Random Forest Model Processes Input
+        2️⃣ Random Forest Model Processes Input
 
-    ⬇
+                    ⬇
 
-    3️⃣ Model Predicts Like / Dislike
+        3️⃣ Model Predicts Like / Dislike
 
-    ⬇
+                    ⬇
 
-    4️⃣ System Displays Recommendation
-    """)
+        4️⃣ System Displays Recommendation
+                        """)
 
 
 
